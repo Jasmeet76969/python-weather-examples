d3c72836d25f1a16e75ef710445a594a598af796
Jasmeet76969 <jasmeet6146@gmail.com>
2026-07-26 19:22:10 +0530

    Update weather with tk.py

--- weather with tk.py
+++ weather with tk.py
@@ -5,7 +5,7 @@ from tkinter import*
 
 def get_weather():
     city1=city_entry.get()
-    api_key="42412a71ec9331ab7c1e04fec97602d3"
+    api_key=""
     url="https://api.openweathermap.org/data/2.5/weather?"
     complete_url=url+"appid="+api_key+"&q="+city1
     res=requests.get(complete_url)
